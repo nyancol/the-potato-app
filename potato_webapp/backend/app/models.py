77c190c743a1678c5239a7e28caeb06513c4125f
I501383 <y.colina@sap.com>
2024-01-16 13:30:14 +0100

    Adding other entities pokemon, spirit animal

--- potato_webapp/backend/app/models.py
+++ potato_webapp/backend/app/models.py
@@ -1,4 +1,8 @@
+from typing import List
+
 from app import db
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import mapped_column, Mapped
 from graphene_sqlalchemy import SQLAlchemyObjectType
 
 
@@ -13,9 +17,15 @@ class Patate(db.Model):
     birth_city = db.Column(db.String())
     gender = db.Column(db.String())
     email = db.Column(db.String())
+    phone = db.Column(db.String())
+
+    locations: Mapped[List["PatateLocation"]] = db.relationship('PatateLocation')
+    pokemons: Mapped[List["Pokemon"]] = db.relationship('Pokemon')
+    spirit_animal: Mapped["SpiritAnimal"] = db.relationship('SpiritAnimal')
+
 
     def __init__(self, first_name, last_name, birth_date, birth_country,
-                 birth_city, gender, email):
+                 birth_city, gender, email, phone):
         self.first_name = first_name
         self.last_name = last_name
         self.birth_date = birth_date
@@ -23,6 +33,79 @@ class Patate(db.Model):
         self.birth_city = birth_city
         self.gender = gender
         self.email = email
+        self.phone = phone
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+
+class PatateLocation(db.Model):
+    __tablename__ = "patate_locations"
+
+    id = db.Column(db.Integer, primary_key=True)
+    patate_id = mapped_column(ForeignKey("patates.id"))
+    country = db.Column(db.String())
+    city = db.Column(db.String())
+    address = db.Column(db.String())
+    post_code = db.Column(db.String())
+    from_timestamp = db.Column(db.Date())
+    to_timestamp = db.Column(db.Date())
+
+    def __init__(self, patate_id, from_timestamp, to_timestamp,
+                 city, country, address, post_code):
+        self.patate_id = patate_id
+        self.from_timestamp = from_timestamp
+        self.to_timestamp = to_timestamp
+        self.city = city
+        self.country = country
+        self.address = address
+        self.post_code = post_code
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+
+class Pokemon(db.Model):
+    __tablename__ = "pokemon"
+
+    id = db.Column(db.Integer, primary_key=True)
+    patate_id = mapped_column(ForeignKey("patates.id"))
+    name = db.Column(db.String())
+    description = db.Column(db.String())
+    L1_image_path = db.Column(db.String())
+    L2_image_path = db.Column(db.String())
+    L3_image_path = db.Column(db.String())
+
+    def __init__(self, patate_id, name, L1_image_description,
+                 L2_image_description, L3_image_description,
+                 L1_image_path, L2_image_path, L3_image_path):
+        self.patate_id = patate_id
+        self.name = name
+        self.L1_image_description = L1_image_description
+        self.L2_image_description = L2_image_description
+        self.L3_image_description = L3_image_description
+        self.L1_image_path = L1_image_path
+        self.L2_image_path = L2_image_path
+        self.L3_image_path = L3_image_path
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+
+class SpiritAnimal(db.Model):
+    __tablename__ = "spirit_animal"
+
+    id = db.Column(db.Integer, primary_key=True)
+    patate_id = mapped_column(ForeignKey("patates.id"))
+    name = db.Column(db.String())
+    description = db.Column(db.String())
+    image_path = db.Column(db.String())
+
+    def __init__(self, patate_id, name, description, image_path):
+        self.patate_id = patate_id
+        self.name = name
+        self.description = description
+        self.image_path = image_path
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
