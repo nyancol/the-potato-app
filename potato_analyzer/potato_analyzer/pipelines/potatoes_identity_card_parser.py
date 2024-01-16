ac0efa0d6bda239a28f20891112f90416e8a4fb9
I501383 <y.colina@sap.com>
2024-01-16 13:29:14 +0100

    Adding new fields to patates card

--- potato_analyzer/potato_analyzer/pipelines/potatoes_identity_card_parser.py
+++ potato_analyzer/potato_analyzer/pipelines/potatoes_identity_card_parser.py
@@ -4,7 +4,18 @@ from deltalake import write_deltalake, DeltaTable
 import subprocess as sp
 import json
 
-card_schema = pa.schema([
+potato_location_schema = pa.schema([
+            ("first_name", pa.string()),
+            ("last_name", pa.string()),
+            ("country", pa.string()),
+            ("city", pa.string()),
+            ("address", pa.string()),
+            ("post_code", pa.string()),
+            ("from_timestamp", pa.date32()),
+            ("to_timestamp", pa.date32()),
+])
+
+potato_schema = pa.schema([
             ("first_name", pa.string()),
             ("last_name", pa.string()),
             ("gender", pa.string()),
@@ -13,14 +24,28 @@ card_schema = pa.schema([
             ("birth_city", pa.string()),
             ("nationalities", pa.list_(pa.string())),
             ("email", pa.string()),
+            ("phone", pa.string()),
         ])
 
-def main():
-    df = pd.read_csv("../rawdata/potatoes_identity_card.csv", sep=";")
+def potatoes_card():
+    df = pd.read_csv("../rawdata/potatoes_identity_card.csv", sep=";",
+                     dtype={"phone": "string"})
     df["birth_date"] = pd.to_datetime(df["birth_date"], dayfirst=True)
     df["nationalities"] = df["nationalities"].apply(lambda n: n.split(','))
     print(df)
-    write_deltalake("../store/potatoes_card", df, schema=card_schema, mode='overwrite', overwrite_schema=True)
+    write_deltalake("../store/potatoes_card", df, schema=potato_schema,
+                    mode='overwrite', overwrite_schema=True)
+
+
+def potato_locations():
+    df = pd.read_csv("../rawdata/potato_locations.csv", sep=";")
+    df["from_timestamp"] = pd.to_datetime(df["from_timestamp"], dayfirst=True)
+    df["to_timestamp"] = pd.to_datetime(df["to_timestamp"], dayfirst=True)
+    print(df)
+    write_deltalake("../store/potato_locations", df, schema=potato_location_schema,
+                    mode='overwrite', overwrite_schema=True)
+
 
 if __name__ == "__main__":
-    main()
+    potatoes_card()
+    potato_locations()
