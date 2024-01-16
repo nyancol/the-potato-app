9756e75e2beddd862a7ab32ff72cf0b6da5ba7d5
I501383 <y.colina@sap.com>
2024-01-16 13:30:59 +0100

    Adding Annuaire with graphql client

--- potato_webapp/frontend/src/components/AnnuaireGallery.vue
+++ potato_webapp/frontend/src/components/AnnuaireGallery.vue
@@ -1,43 +1,44 @@
 <template>
-  <v-container>
+ <v-container>
     <h2 class="text-h2">Annuaire</h2>
-
-    <!-- Year Selection -->
-    <v-row justify="center" class="mb-4">
-      <v-col cols="12" sm="6" md="4" lg="3">
-        <v-select
-          v-model="selectedYear"
-          :items="years"
-          label="Select Year"
-          @change="fetchImageData"
-        ></v-select>
-      </v-col>
-    </v-row>
-  
     <v-row justify="center">
-      <v-col v-for="(image, index) in images" :key="index" cols="12" sm="6" md="6" lg="4" xl="3">
+      <v-col v-for="patate in patates" :key="patate.id" cols="12" sm="6" md="6" lg="4" xl="3">
         <v-card class="country-card">
-          <v-img :src="image.url" alt="Image" class="gallery-image"></v-img>
+          <v-img :src="getImageUrl(patate.spiritAnimal.imagePath)" alt="Image" class="gallery-image"></v-img>
           <v-divider></v-divider>
           <v-list>
             <v-list-item>
               <v-list-item-content>
                 <v-list-item-title class="info-text">
-                  <v-icon class="country-icon">mdi-account</v-icon> {{ image.first_name }} {{ image.last_name }}
+                  <v-icon class="country-icon">mdi-account</v-icon> {{ patate.firstName }} {{ patate.lastName }}
                 </v-list-item-title>
               </v-list-item-content>
             </v-list-item>
             <v-list-item>
               <v-list-item-content>
                 <v-list-item-title class="info-text">
-                  <v-icon class="country-icon">mdi-paw</v-icon> {{ image.spirit_animal }}
+                  <v-icon class="country-icon">mdi-phone</v-icon> {{ patate.phone }}
                 </v-list-item-title>
               </v-list-item-content>
             </v-list-item>
             <v-list-item>
               <v-list-item-content>
                 <v-list-item-title class="info-text description-text">
-                  <v-icon class="country-icon">mdi-book-open-variant</v-icon> {{ image.spirit_animal_description }}
+                  <v-icon class="country-icon">mdi-email</v-icon> {{ patate.email }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title class="info-text description-text">
+                  <v-icon class="country-icon">mdi-city</v-icon> {{ patate.locations[0].city }} {{ patate.locations[0].postCode }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title class="info-text description-text">
+                  <v-icon class="country-icon">mdi-map-marker</v-icon> {{ patate.locations[0].address }}
                 </v-list-item-title>
               </v-list-item-content>
             </v-list-item>
@@ -49,69 +50,71 @@
 </template>
 
 <script>
+import { watch, computed } from 'vue'
+import { useQuery } from '@vue/apollo-composable'
+import gql from 'graphql-tag'
+
 export default {
-  data() {
-    return {
-      selectedYear: null,
-      years: ['2019', '2020', '2021', '2022', '2023'],
-      images: [],
-    };
-  },
-  mounted() {
-    // Default to the first year
-    this.selectedYear = this.years[0];
-    this.fetchImageData();
-  },
-  watch: {
-    selectedYear: 'fetchImageData', // Watch for changes in selectedYear
-  },
-  methods: {
-    fetchImageData() {
-      const backendUrl = 'http://localhost:5000';
-      const apiUrl = `${backendUrl}/api/images/data?year=${this.selectedYear}`;
-      
-      fetch(apiUrl)
-        .then(response => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
+  setup() {
+    const { result } = useQuery(gql`query patates {
+          patates {
+            id
+            firstName
+            lastName
+            phone
+            email
+            spiritAnimal {
+              name
+              imagePath
+            }
+            locations {
+              country
+              city
+              address
+              postCode
+            }
           }
-          return response.json();
-        })
-        .then(imageData => {
-          this.images = imageData.map(({ filename, first_name, last_name, spirit_animal, spirit_animal_description }) => ({
-            url: this.getImageUrl(backendUrl, filename),
-            first_name,
-            last_name,
-            spirit_animal,
-            spirit_animal_description,
-          }));
-        })
-        .catch(error => {
-          console.error('Error fetching image data:', error);
-        });
-    },
-    getImageUrl(backendUrl, filename) {
-      return `${backendUrl}/api/images/${encodeURIComponent(filename)}`;
-    },
+        }
+        `)
+
+    watch(() => {
+      console.log(result.value)
+    })
+
+    const backendUrl = 'http://localhost:5000';
+    const getImageUrl = (imagePath) => {
+      return `${backendUrl}/api/images/${imagePath.split("/")[3]}`; // encodeURIComponent(
+    };
+
+    const patates = computed(() => result.value?.patates ?? [])
+
+    return {
+      patates,
+      getImageUrl
+    };
   },
 };
 </script>
 
 <style scoped>
 .country-card {
-  background-color: #f8f1d2; /* Light yellow background */
-  border: 2px solid #7d6544; /* Brown border */
+  background-color: #f8f1d2;
+  /* Light yellow background */
+  border: 2px solid #7d6544;
+  /* Brown border */
   border-radius: 10px;
   overflow: hidden;
   transition: background-color 0.3s ease;
 }
 
 .country-card:hover {
-  background-color: #ffe4b5; /* Hover color change */
+  background-color: #ffe4b5;
+  /* Hover color change */
 }
 
 .country-icon {
-  color: #7d6544; /* Brown color for icons */
+  color: #7d6544;
+  /* Brown color for icons */
 }
 
 .image-gallery {
@@ -139,7 +142,8 @@ export default {
 
 .gallery-image {
   width: 100%;
-  max-width: 100%; /* Full width for larger images */
+  max-width: 100%;
+  /* Full width for larger images */
   height: auto;
   border-radius: 5px;
 }
@@ -151,11 +155,13 @@ export default {
 
 .info-text {
   margin: 5px 0;
-  font-size: 16px; /* Increase font size for better readability */
+  font-size: 16px;
+  /* Increase font size for better readability */
   color: #555;
 }
 
 .description-text {
-  white-space: pre-wrap; /* Allow line breaks */
+  white-space: pre-wrap;
+  /* Allow line breaks */
 }
 </style>
--- potato_webapp/frontend/src/main.js
+++ potato_webapp/frontend/src/main.js
@@ -1,12 +1,42 @@
-import { createApp } from "vue";
+import { createApp, provide, h } from "vue";
 import App from "./App.vue";
+
+import { ApolloClient, InMemoryCache } from '@apollo/client/core'
+import { createApolloProvider } from '@vue/apollo-option'
+import VueApolloComponents from '@vue/apollo-components'
+
+import { DefaultApolloClient } from '@vue/apollo-composable'
+
 import { createRouter, createWebHistory } from "vue-router";
+
 import vuetify from "./plugins/vuetify";
+
 import HomePage from "./views/HomePage.vue";
 import AnnuaireGallery from "./components/AnnuaireGallery.vue";
 import PokedexGallery from "./components/PokedexGallery.vue";
 
-const app = createApp(App);
+
+const cache = new InMemoryCache()
+
+const apolloClient = new ApolloClient({
+  cache,
+  uri: 'http://localhost:5000/graphql',
+})
+
+const apolloProvider = createApolloProvider({
+  defaultClient: apolloClient,
+})
+
+
+const app = createApp({
+  setup () {
+    provide(DefaultApolloClient, apolloClient)
+  },
+
+  render: () => h(App),
+})
+
+app.use(apolloProvider)
 
 // Use Vue Router
 const router = createRouter({
@@ -20,8 +50,6 @@ const router = createRouter({
 });
 
 app.use(router);
-
-// Use Vuetify
+app.use(VueApolloComponents)
 app.use(vuetify);
-
 app.mount("#app");
