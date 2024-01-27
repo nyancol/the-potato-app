f6d9f7764b6319f435307ba17994906773de7dbc
I501383 <y.colina@sap.com>
2024-01-27 11:11:16 +0100

    next.js, prisma, vercel demo

---
+++ webapp/pages/api/post/index.ts
@@ -0,0 +1,22 @@
+import { getSession } from 'next-auth/react';
+import { getServerSession } from "next-auth"
+import prisma from '../../../lib/prisma';
+import { authOptions } from '../../../pages/api/auth/[...nextauth]'
+
+
+// POST /api/post
+// Required fields in body: title
+// Optional fields in body: content
+export default async function handle(req, res) {
+  const { title, content } = req.body;
+
+  const session = await getServerSession(req, res, authOptions)
+  const result = await prisma.post.create({
+    data: {
+      title: title,
+      content: content,
+      author: { connect: { email: session?.user?.email } },
+    },
+  });
+  res.json(result);
+}
